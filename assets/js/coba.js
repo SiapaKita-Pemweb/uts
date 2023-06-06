27f6d2d71d925676d4049d267556def49ed1060e
naufal badri <badrinaufal@gmail.com>
2023-06-06 12:05:49 +0700

    first commit

---
+++ assets/js/coba.js
@@ -0,0 +1,71 @@
+// var slideIndex = 1;
+//       showSlides(slideIndex);
+
+//       // Fungsi untuk menampilkan slide berikutnya/sebelumnya
+//       function plusSlides(n) {
+//         showSlides(slideIndex += n);
+//       }
+
+//       // Fungsi untuk menampilkan slide saat ini
+//       function currentSlide(n) {
+//         showSlides(slideIndex = n);
+//       }
+
+//       function showSlides(n) {
+//         var i;
+//         var slides = document.getElementsByClassName("mySlides");
+//         var dots = document.getElementsByClassName("dot");
+//         if (n > slides.length) {
+//           slideIndex = 1;
+//         }
+//         if (n < 1) {
+//           slideIndex = slides.length;
+//         }
+//         for (i = 0; i < slides.length; i++) {
+//           slides[i].style.display = "none";
+//         }
+//         for (i = 0; i < dots.length; i++) {
+//           dots[i].className = dots[i].className.replace(" active", "");
+//         }
+//         slides[slideIndex - 1].style.display = "block";
+//         dots[slideIndex - 1].className += " active";
+//       }
+
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if(window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
